db99817dc1d5ba8797b8f71d3eac3c9bc35499e7
noraiz-anwar <noraizbhatti@gmail.com>
2018-07-11 11:05:16 +0500

    add python_lib getter in capa system

--- common/lib/xmodule/xmodule/capa_module.py
+++ common/lib/xmodule/xmodule/capa_module.py
@@ -13,6 +13,8 @@ from xmodule.exceptions import NotFoundError, ProcessingError
 from xmodule.raw_module import RawDescriptor
 from xmodule.util.misc import escape_html_characters
 from xmodule.x_module import DEPRECATION_VSCOMPAT_EVENT, XModule, module_attr
+from xmodule.contentstore.django import contentstore
+from common.djangoapps.util.sandboxing import get_python_lib_zip
 
 from .capa_base import CapaFields, CapaMixin, ComplexEncoder
 
@@ -341,7 +343,7 @@ class CapaDescriptor(CapaFields, RawDescriptor):
             anonymous_student_id=None,
             cache=None,
             can_execute_unsafe_code=lambda: None,
-            get_python_lib_zip=lambda: None,
+            get_python_lib_zip=(lambda: get_python_lib_zip(contentstore, self.runtime.course_id)),
             DEBUG=None,
             filestore=self.runtime.resources_fs,
             i18n=self.runtime.service(self, "i18n"),
